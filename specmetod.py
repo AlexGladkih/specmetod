e78804e31016a5b42fb5ea627967dc366d6feffe
AlexGladkih <167768609+AlexGladkih@users.noreply.github.com>
2024-06-18 22:58:23 +0800

    Initial commit

---
+++ specmetod.py
@@ -0,0 +1,9 @@
+class Hause:
+    def __init__(self):
+         self. numberOfFloors = 0
+    def setNewNumberOfFloors(self, floors):
+        self. numberOfFloors = floors
+        print(self. numberOfFloors)
+
+house = Hause()
+house.setNewNumberOfFloors(10)
